a2cf43d3be53cc683e3cd9f2304922c652740dee
Filippo Manzini <filippomanzini02@outlook.com>
2026-04-21 22:44:06 +0200

    centralizzazione plugin per invio mail private

--- strapi/config/plugins.js
+++ strapi/config/plugins.js
@@ -17,17 +17,17 @@ module.exports = () => ({
     config: {
       provider: 'nodemailer',
       providerOptions: {
-        host: process.env.SMTP_HOST || 'smtp.mailersend.net',
+        host: process.env.SMTP_HOST,
         port: parseInt(process.env.SMTP_PORT || '587', 10),
-        secure: process.env.SMTP_SECURE === 'true',
+        secure: process.env.SMTP_SECURE,
         auth:{
           user: process.env.SMTP_USER,
           pass: process.env.SMTP_PASS,
         }
       },
       settings: {
-        defaultFrom: process.env.SMTP_DEFAULT_FROM || 'no-reply@example.com',
-        defaultReplyTo: process.env.SMTP_DEFAULT_REPLY_TO || 'support@example.com',
+        defaultFrom: process.env.SMTP_DEFAULT_FROM,
+        defaultReplyTo: process.env.SMTP_DEFAULT_REPLY_TO,
       },
     },
   },
