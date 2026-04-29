fe2df27dbe7cbcaa17073058638692b8b584d160
bscmtt <mattia.boschini25@gmail.com>
2026-04-29 11:13:10 +0200

    deploy funzionante

--- strapi/config/plugins.js
+++ strapi/config/plugins.js
@@ -19,7 +19,7 @@ module.exports = () => ({
       providerOptions: {
         host: process.env.SMTP_HOST,
         port: parseInt(process.env.SMTP_PORT || '587', 10),
-        secure: process.env.SMTP_SECURE,
+        secure: process.env.SMTP_SECURE === 'true',
         auth:{
           user: process.env.SMTP_USER,
           pass: process.env.SMTP_PASS,
